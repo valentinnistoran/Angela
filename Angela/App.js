0e06c4bb596eb7bda58e523554cd3e0540015094
valentinnistoran <valentin.nistoran@student.upt.ro>
2024-01-27 18:50:56 +0200

    Initialized the React Native project

---
+++ Angela/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/Home.js';
+import ProfileScreen from './screens/Profile.js';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
